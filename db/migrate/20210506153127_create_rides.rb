ceeccab92cd81a31fd1d7a54f939c57c4c6061d2
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-02 13:36:05 -0500

    commit tree e0a29d6f0fad9b34b81637c8f0429ed08ceb3803

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,9 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.belongs_to :taxi
+      t.belongs_to :passenger
+      t.datetime :rides_date
       t.timestamps
     end
   end
